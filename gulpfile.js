eacc0fd60d57a8b1249b0f4a5aa65e93fe466e54
yang yu <yundkyy@gmail.com>
2014-11-10 12:41:47 +0800

    gulp 构建项目的基础，jshint clean replace。

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,15 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-
+var htmlreplace = require('gulp-html-replace');
 // 检查脚本
 gulp.task('lint', function() {
     gulp.src('./js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
-
+var buildType = "dev"
+var defaultType= "default_"+buildType// 
 // 编译Sass
 // gulp.task('sass', function() {
 //     gulp.src('./scss/*.scss')
@@ -31,13 +32,32 @@ gulp.task('scripts', function() {
         .pipe(uglify())
         .pipe(gulp.dest('./dist'));
 });
+// 将bower的库文件对应到指定位置
+gulp.task('buildlib',function(){
+
+  gulp.src('./bower_components/jquery/dist/jquery.min.js')
+      .pipe(gulp.dest('./app/build/lib/js/'))
+});
+// 将html主文件生成合适的版本并放到合适的地方
+var buildhtmlType = 'buildhtml_'+buildType;
+gulp.task('buildhtml', [buildhtmlType]);
+gulp.task('buildhtml_dev',function(){
+
+  gulp.src('index.html')
+    .pipe(htmlreplace({
+        'css': ['styles.min.css','hehe.css'],
+        'js': 'main.js'
+    }))
+      .pipe(gulp.dest('./app/'))
+});
 
 // 默认任务
-gulp.task('default', function(){
-    gulp.run('lint', 'scripts');
+gulp.task('default_dev', function(){
+    gulp.run('lint', 'scripts','buildlib','buildhtml');
 
     // 监听文件变化
     gulp.watch('./js/*.js', function(){
         gulp.run('lint', 'scripts');
     });
 });
+gulp.task('default', [defaultType]);
--- main.js
+++ main.js
@@ -1,11 +1,3 @@
 $(function(){
-	 $.ajax( {
-                        url: "http://192.168.1.105:3000/rest",
-                        type: "get",
-                        contentType:"application/x-www-form-urlencoded; charset=utf-8",
-                        success : function (result) {
-
-                       		console.log(result);
-                        }
-                    });
+	alert("ok,haha")
 })
