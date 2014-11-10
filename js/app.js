9c91e421e2c34f5dc6d179c6944808e9b02d2b2a
yang yu <yundkyy@gmail.com>
2014-11-10 17:18:49 +0800

    $http的使用

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 var app = angular.module('linktalk', []);
-app.controller('bookController', ['$scope', function($scope) {
+app.controller('bookController', ['$scope','$http', function($scope,$http) {
     $scope.todos = [
       {text:'learn angular', done:true},
       {text:'build an angular app', done:false}];
  
     $scope.allbooks = function() {
-      alert("ok")
+       $http({
+       	method: 'get', 
+       	url: "http://192.168.1.105:3000/rest",
+
+       }).
+        success(function(data, status) {
+          $scope.status = status;
+          $scope.data = data;
+          console.log(data)
+        }).
+        error(function(data, status) {
+          $scope.data = data || "Request failed";
+          $scope.status = status;
+          console.log(status);
+      });
     };
 }]);
